a843a6aa644dc0d167ac14014a375ff12378bcad
d.tkachenko <d.tkachenko@argustelecom.ru>
2023-05-10 20:39:31 +0300

    commit

--- src/main/java/com/example/myfirstjpaproject/BookController.java
+++ src/main/java/com/example/myfirstjpaproject/BookController.java
@@ -1,14 +1,22 @@
 package com.example.myfirstjpaproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
 
+    @Autowired
+    BooksRepository booksRepository;
+
     @GetMapping(value = "/hello")
     public void method() {
-
+        Book book = new Book();
+        book.setId(1);
+        book.setName("Чистый код");
+        book.setAuthorName("Роберт Мартин");
+        booksRepository.save(book);
     }
 
 
